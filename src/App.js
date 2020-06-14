5ff45edfc54590a75a85e31ba1dcac96f5379c83
darshan bordekar <darshanbordekar1819@gmail.com>
2020-06-15 00:11:54 +0530

    [ Darshan ]  | Navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Lifecycle from "./Components/AboutLife/Lifecycle";
+import Navigation from "./Components/Navigation/Navigation";
 
 function App() {
   return (
     <div className="App">
+      <Navigation></Navigation>
       <Lifecycle></Lifecycle>
     </div>
   );
