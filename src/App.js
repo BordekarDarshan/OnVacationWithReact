38745869636eed7ba4f88cb0d86b3847935ff64a
darshan bordekar <darshanbordekar1819@gmail.com>
2020-06-15 00:22:52 +0530

    [ Darshan ] | Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from "react";
-import Lifecycle from "./Components/AboutLife/Lifecycle";
-import Navigation from "./Components/Navigation/Navigation";
 
 function App() {
-  return (
-    <div className="App">
-      <Navigation></Navigation>
-      <Lifecycle></Lifecycle>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
