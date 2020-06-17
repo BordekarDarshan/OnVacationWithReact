a66e0e48f82957a96291ad98c70883e3ec1ef781
darshan bordekar <darshanbordekar1819@gmail.com>
2020-06-18 01:12:06 +0530

    [ Darshan ] | Authentication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import HomePage from "./pages/HomePage/HomePage";
 import Navigation from "./components/Navigation/Navigation";
 import { Route, Switch } from "react-router-dom";
 import HatsPage from "./pages/HatsPage/HatsPage";
 import ShopPage from "./pages/ShopPage/ShopPage";
 import SignInAndSignUpPage from "./pages/SignInAndSignUpPage/SignInAndSignUpPage";
+import { auth } from "./Firebase/Firebase";
 
-function App() {
-  return (
-    <React.Fragment>
-      <Navigation></Navigation>
-      <Switch>
-        <Route path="/" exact component={HomePage}></Route>
-        <Route path="/hats" component={HatsPage}></Route>
-        <Route path="/shop" component={ShopPage}></Route>
-        <Route path="/signin" component={SignInAndSignUpPage}></Route>
-      </Switch>
-    </React.Fragment>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+      console.log(this.state.currentUser);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Navigation currentUser={this.state.currentUser}></Navigation>
+        <Switch>
+          <Route path="/" exact component={HomePage}></Route>
+          <Route path="/hats" component={HatsPage}></Route>
+          <Route path="/shop" component={ShopPage}></Route>
+          <Route path="/signin" component={SignInAndSignUpPage}></Route>
+        </Switch>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
-import "./Navigation.css";
+import { auth } from "../../Firebase/Firebase";
 import { Link } from "react-router-dom";
+import "./Navigation.css";
 
 export class Navigation extends Component {
   render() {
+    const { currentUser } = this.props;
     return (
       <div className="header ">
         <div className="logo">
@@ -16,10 +18,15 @@ export class Navigation extends Component {
               SHOP
             </Link>
           </li>
-
-          <Link to="/signin" className="userLogin">
-            <li>SIGN IN</li>
-          </Link>
+          {currentUser ? (
+            <li onClick={() => auth.signOut()} className="userLogout">
+              SIGN Out
+            </li>
+          ) : (
+            <Link to="/signin" className="userLogin">
+              <li>SIGN IN</li>
+            </Link>
+          )}
 
           <li>YOUR CART</li>
         </ul>
