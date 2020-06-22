a10960c5ecee3e1eeccf88553c583002d428198f
darshan bordekar <darshanbordekar1819@gmail.com>
2020-06-22 13:41:21 +0530

    [ Darshan ] | Feture for empty cart

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -6,19 +6,33 @@ import CartItemList from "../CartItemList/CartItemList";
 function CartDropdown({ goToCheckout, cartList }) {
   return (
     <div className="cartDropDownWrapper">
-      <div className="cartItemsWrapper">
-        {cartList
-          ? cartList.map((item) => <CartItemList key={item.id} {...item} />)
-          : null}
-      </div>
-      <button
-        type="button"
-        className="btn btnCheckOut"
-        style={{ backgroundColor: "#05192d", color: "white" }}
-        onClick={goToCheckout}
-      >
-        GO TO CHECKOUT
-      </button>
+      {cartList.length === 0 ? (
+        <div className="noCartItems">
+          <img
+            src="https://i.ibb.co/2PZ87gW/Empty-Bag.png"
+            alt="Empty Cart"
+          ></img>
+          <p className="customP">
+            There is nothing in your bag. Let's add some items
+          </p>
+        </div>
+      ) : (
+        <React.Fragment>
+          <div className="cartItemsWrapper">
+            {cartList
+              ? cartList.map((item) => <CartItemList key={item.id} {...item} />)
+              : null}
+          </div>
+          <button
+            type="button"
+            className="btn btnCheckOut"
+            style={{ backgroundColor: "#05192d", color: "white" }}
+            onClick={goToCheckout}
+          >
+            GO TO CHECKOUT
+          </button>
+        </React.Fragment>
+      )}
     </div>
   );
 }
