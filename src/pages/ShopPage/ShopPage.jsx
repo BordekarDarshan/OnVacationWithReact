291fd08f9eca776f6c0033d9bdddc13ad7f4663b
darshan bordekar <darshanbordekar1819@gmail.com>
2020-07-08 00:30:09 +0530

    [ Darshan ] | Revert Point | Code Before implementing firebase in redux

--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -1,22 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import CollectionOverview from "../../components/CollectionOverview/CollectionOverview";
 import Collection from "../Collection/Collection";
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../Firebase/Firebase";
+export class ShopPage extends Component {
+  unsubscribeFromSnapshot = null;
 
-function ShopPage({ match }) {
-  return (
-    <div className="container-fluid mt-2">
-      <Route
-        exact
-        path={`${match.path}`}
-        component={CollectionOverview}
-      ></Route>
-      <Route
-        path={`${match.path}/:collectionName`}
-        component={Collection}
-      ></Route>
-    </div>
-  );
+  componentDidMount() {
+    const CollectionRef = firestore.collection("collections");
+    // whenever collectionRef gets update or run for the first time this CollectionRef wikk send us the snapshot
+    // representing the code of our collection objects array at the time when this code renders.
+    // will get from "onSnapshot" is snapshot object of our collection
+    CollectionRef.onSnapshot(async (snapshot) =>
+      convertCollectionsSnapshotToMap(snapshot)
+    );
+  }
+
+  render() {
+    const { match } = this.props;
+    return (
+      <div className="container-fluid mt-2">
+        <Route
+          exact
+          path={`${match.path}`}
+          component={CollectionOverview}
+        ></Route>
+        <Route
+          path={`${match.path}/:collectionName`}
+          component={Collection}
+        ></Route>
+      </div>
+    );
+  }
 }
 
 export default ShopPage;
